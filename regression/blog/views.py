1c77e12bfd1f2e830851a0d29f677a067e38b5e0
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-28 22:11:27 -0500

    [sm] created posts in the blog views folder and passed them to the home route via context

--- regression/blog/views.py
+++ regression/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'auther': 'Stuart Marsh',
+        'title': 'blog post one',
+        'content': 'First post content',
+        'date_posted': 'Auguest 23, 2018'
+    },
+    {
+        'auther': 'Ally Brisbin',
+        'title': 'blog post two',
+        'content': 'Second post content',
+        'date_posted': 'Auguest 23, 2018'
+    },
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')

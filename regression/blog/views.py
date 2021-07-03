ce23d255fdac81a06e52331d50d70ce6301e8cc7
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-07-02 22:58:01 -0500

    [sm] commented out old dummy data of posts in views. Added comma in home.html to date format

--- regression/blog/views.py
+++ regression/blog/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
-posts = [
-    {
-        'author': 'Stuart Marsh',
-        'title': 'blog post one',
-        'content': 'First post content',
-        'date_posted': 'August 10, 2020'
-    },
-    {
-        'author': 'Ally Brisbin',
-        'title': 'blog post two',
-        'content': 'Second post content',
-        'date_posted': 'August 24, 2019'
-    },
-]
+# posts = [
+#     {
+#         'author': 'Stuart Marsh',
+#         'title': 'blog post one',
+#         'content': 'First post content',
+#         'date_posted': 'August 10, 2020'
+#     },
+#     {
+#         'author': 'Ally Brisbin',
+#         'title': 'blog post two',
+#         'content': 'Second post content',
+#         'date_posted': 'August 24, 2019'
+#     },
+# ]
 
 # Create your views here.
 def home(request):
